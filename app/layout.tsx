d17d321a84c06b1fa573789094b46732996d986f
Richard William <rwilliam.flores@gmail.com>
2026-01-05 16:42:45 +0800

    feat: update font settings to include Outfit font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Outfit } from "next/font/google";
 import TopLoaderWrapper from "@/components/wrappers/toploader-wrapper";
 
 import "./globals.css";
@@ -11,6 +11,11 @@ const geistSans = Geist({
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const outfit = Outfit({
+  variable: "--font-outfit",
   subsets: ["latin"],
 });
 
@@ -27,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${geistSans.className} antialiased min-h-screen bg-background`}
+        className={`${geistSans.variable} ${geistMono.variable} ${outfit.variable} ${outfit.className} antialiased min-h-screen bg-background`}
       >
         <TopLoaderWrapper>{children}</TopLoaderWrapper>
       </body>
